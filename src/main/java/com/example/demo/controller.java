9ae2b79a662f17aba843447862a375e19aaa7ea8
Belal <belalansari087@gmail.com>
2017-04-21 13:03:46 +0530

    keycloak added

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public Demo create(@RequestBody Demo demo)
+	public Demo create(KeycloakPrincipal<RefreshableKeycloakSecurityContext> principal, @RequestBody Demo demo)
 	{
+		String email = principal.getKeycloakSecurityContext().getToken().getEmail();
+		demo.setEmail(email);
 		return demo;
+	}
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String get(KeycloakPrincipal<RefreshableKeycloakSecurityContext> principal)
+	{
+		String email = principal.getKeycloakSecurityContext().getToken().getEmail();
+		return email;
 	}
 }
